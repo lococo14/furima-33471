81edcc3718fce24b169b82863f2970c9bf1fa532
lococo14 <70187032+lococo14@users.noreply.github.com>
2021-01-15 15:48:04 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
+ 
+  validates :category_id, :condition_id, :shipping_date_id, :shipping_payer_id, :prefecture_id, numericality: { other_than: 1 }
+  
   belongs_to :user 
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_payer
+  belongs_to :shipping_date
+  belongs_to :prefecture 
 end
