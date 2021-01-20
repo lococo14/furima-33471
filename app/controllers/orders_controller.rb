c8a896e26d3cc2dda522674da78145d9f7ccf78f
lococo14 <70187032+lococo14@users.noreply.github.com>
2021-01-20 15:47:09 +0900

    購入記録と送付先のテーブル保存

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,24 @@
+class OrdersController < ApplicationController
+
+  def index
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+
+  end
+  
+  def create
+    @order_address = OrderAddress.new(address_params)
+    if @order_address.valid?
+       @order_address.save
+      redirect_to root_path
+    else  
+      render action: :index
+    end      
+  end  
+
+  private
+    def address_params
+      params.require(:order_address).permit(:zip_code, :prefecture_id,:city,:street_name,:building_name,:phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    end   
+
+end
