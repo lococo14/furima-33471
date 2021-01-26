973012c091240201cc7ca7feed275e0497a65807
lococo14 <70187032+lococo14@users.noreply.github.com>
2021-01-27 00:48:17 +0900

    herokuデプロイ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "wss://furima-33471.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://furima-33471.herokuapp.com', 'http://furima-33471.herokuapp.com']
 
+ 
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
